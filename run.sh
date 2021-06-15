acedcb8be56d0b157bb532c7f774f80fc5974a1b
Jo Power Tech <83786289+JoPowerTech@users.noreply.github.com>
2021-06-15 13:15:17 -0300

    Run
    Removendo o motd

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@ clear
 
 echo "Aguarde..."
 echo ""
+sleep 1.5
 
 cd; cd .termux
 rm -rf colors.properties; cd; cd TermuxColorScheme
 mv colors.properties /data/data/com.termux/files/home/.termux
 cd; cd ..; cd usr; cd etc
+rm -rf motd
 rm -rf bash.bashrc
 cd; cd TermuxColorScheme
 mv bash.bashrc /data/data/com.termux/files/usr/etc
